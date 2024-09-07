077f1e60c4c3bf57dab2e0c588bf5dfe1acfc671
Chestnut45 <cpt20xx@gmail.com>
2024-09-07 17:55:19 -0300

    Separate GameObject from Scene in wolf (inner class prevented easy reuse and specialization)

---
+++ wolf/W_GameObject.cpp
@@ -0,0 +1,45 @@
+#include "W_GameObject.h"
+
+namespace wolf
+{
+
+GameObject::GameObject(Scene& scene, GameObjectID id)
+    : m_scene(scene), m_id(id)
+{
+}
+
+GameObject::~GameObject()
+{
+}
+
+void GameObject::AddChild(GameObject& object)
+{
+    // Get the object's parent
+    GameObject* parent = object.GetParent();
+    if (parent)
+    {
+        // Early out if the object is already one of our children
+        if (parent == this) return;
+
+        // Remove existing relationship
+        parent->RemoveChild(object);
+    }
+
+    // Update references
+    m_children.push_back(&object);
+    object.m_parent = this;
+}
+
+void GameObject::RemoveChild(GameObject& object)
+{
+    // Find the child object in our list of children
+    const auto& it = std::find(m_children.begin(), m_children.end(), &object);
+    if (it != m_children.end())
+    {
+        // If found, remove and update child's parent
+        object.m_parent = nullptr;
+        m_children.erase(it);
+    }
+}
+
+};
---
+++ wolf/W_GameObject.h
@@ -0,0 +1,130 @@
+#pragma once
+
+//-----------------------------------------------------------------------------
+// File:			W_GameObject.h
+// Original Author:	D'Anyil Landry
+//
+// A class representing a game object that belongs to a scene. Game objects
+// can have components of any type attached to them, and can form hierarchical
+// relationships with other game objects within the same scene.
+//-----------------------------------------------------------------------------
+
+#include <cstdint>
+
+#include "W_Scene.h"
+
+namespace wolf
+{
+
+class GameObject
+{
+// Public interface
+public:
+
+    ~GameObject();
+
+    // Delete copy constructor/assignment
+    GameObject(const GameObject&) = delete;
+    GameObject& operator=(const GameObject&) = delete;
+
+    // Delete move constructor/assignment
+    GameObject(GameObject&& other) = delete;
+    GameObject& operator=(GameObject&& other) = delete;
+
+    // General management
+
+    // Retrieve the ID of this game object.
+    inline GameObjectID GetID() { return m_id; }
+
+    // Retrieve a reference to the scene this game object belongs to
+    inline Scene& GetScene() { return m_scene; }
+
+    // Deletes this game object and all of its components from the scene
+    inline void Delete() { m_scene.DeleteObject(m_id); }
+
+    // Component management
+
+    // Adds a component to the game object by in-place construction.
+    // Pass your component's constructor arguments directly to this function!
+    template <typename T, typename... Args>
+    T& AddComponent(Args&&... args)
+    {
+        return m_scene.m_registry.emplace<T>(m_id, args...);
+    }
+
+    // Gets a pointer to the component, or nullptr if it doesn't exist
+    template <typename T>
+    T* GetComponent()
+    {
+        return m_scene.m_registry.try_get<T>(m_id);
+    }
+
+    // Deletes the component from the object, if it exists
+    template <typename T>
+    void DeleteComponent()
+    {
+        m_scene.m_registry.remove<T>(m_id);
+    }
+
+    // Returns true if the object has all of the given components
+    template <typename... T>
+    bool HasAll()
+    {
+        return m_scene.m_registry.all_of<T...>(m_id);
+    }
+    
+    // Returns true if the object has any of the given components
+    template <typename... T>
+    bool HasAny()
+    {
+        return m_scene.m_registry.any_of<T...>(m_id);
+    }
+
+    // Hierarchy management
+    
+    // Adds a game object to our list of children
+    // If the object already has a parent, it is removed from that parent first
+    void AddChild(GameObject& object);
+
+    // Removes the given child object from our list of children, and
+    // updates the child object's parent pointer to be empty
+    void RemoveChild(GameObject& object);
+
+    // Returns a pointer to the parent object, or nullptr if we have none
+    inline GameObject* GetParent() const { return m_parent; }
+
+    // Gets the list of children objects by id
+    inline const std::vector<GameObject*>& GetChildren() const { return m_children; }
+
+    // True if this object has 1 or more children
+    inline bool HasChildren() const { return m_children.size() > 0; }
+
+    // Used internally but must be public due to allocator rules.
+    // NOTE: Don't instantiate Objects directly! Use Scene::CreateObject().
+    GameObject(Scene& scene, GameObjectID id);
+
+// Data / implementation
+private:
+
+    // Non-owning reference to the scene that created this object
+    Scene& m_scene;
+
+    // Our ID within the scene
+    const GameObjectID m_id;
+
+    // Pointer to our parent game object, if any
+    GameObject* m_parent = nullptr;
+
+    // List of child objects
+    std::vector<GameObject*> m_children;
+
+    // This token guarantees pointer stability for objects within the scene.
+    // Add it to your component class if you want to store and reuse pointers
+    // to the component type over multiple frames.
+    static constexpr auto in_place_delete = true;
+
+    // Necessary for the scene to create and delete game objects
+    friend class Scene;
+};
+
+};
