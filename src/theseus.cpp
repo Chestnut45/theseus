cb0ee35f92e38e17f88ad48eec7672c7d65a97c9
Chestnut45 <cpt20xx@gmail.com>
2024-09-17 17:23:18 -0300

    fix compile error

--- src/theseus.cpp
+++ src/theseus.cpp
@@ -86,9 +86,6 @@ void Theseus::Update(float delta)
 
     // Show debug information window
     if (m_showDebug) ShowDebug();
-
-    this->m_pHitboxManager->CheckCollisions();
-    this->m_pHurtboxManager->CheckCollisions();
 }
 
 void Theseus::Render()
