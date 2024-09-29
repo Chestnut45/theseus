b5dbf6acfcb3d634f6d030916665847bc28a361e
Chestnut45 <cpt20xx@gmail.com>
2024-09-29 20:48:00 -0300

    Move labyrinth tile enum to separate file

---
+++ src/LabyrinthTiles.h
@@ -0,0 +1,44 @@
+#pragma once
+
+//-----------------------------------------------------------------------------
+// File:			LabyrinthTiles.h
+// Original Author:	D'Anyil Landry
+//
+// A header declaring an enum to name each Tile ID in the labyrinth.
+//-----------------------------------------------------------------------------
+
+// Labyrinth tile IDs
+// NOTE: Using a normal enum scoped in a struct allows us
+// to use the Tile IDs as integers without casting them.
+struct Tile
+{
+    typedef int type;
+    enum : type
+    {
+        Empty = -1,
+        BorderedGrass = 0,
+        Bricks,
+        FloorSmallSquares,
+        FloorSpiralGold,
+        FloorSpiral,
+        FloorSquareGold,
+        FloorSquare,
+        Grass,
+        WallBottomLeft,
+        WallBottomRight,
+        WallBottom,
+        WallChest,
+        WallHelmet,
+        WallLeft,
+        WallMaze,
+        WallMinotaur,
+        WallPillars,
+        WallPot,
+        WallRight,
+        WallSpiral,
+        WallSquare,
+        WalTopLeft,
+        WallTopRight,
+        WallTop
+    };
+};
