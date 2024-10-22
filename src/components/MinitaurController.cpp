b84f9318545c379bdedde2b3fe98412709da9c2b
tripzcodes <151476551+tripzcodes@users.noreply.github.com>
2024-10-21 21:22:38 -0300

    Modified comments

--- src/components/MinitaurController.cpp
+++ src/components/MinitaurController.cpp
@@ -290,7 +290,7 @@ void MinitaurController::HandleDeathState()
     }
 
     // Destroy the GameObject when the Minitaur dies
-    //will be implemented later
+    //implemented in playstate
 }
 
 void MinitaurController::ChangeState(EnemyState newState)
