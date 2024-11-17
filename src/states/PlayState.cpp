521fd6b304aace2f80a12265fb0ca833e355f7fb
N-M-Nguyen <nmnguyen8464@upei.ca>
2024-11-18 02:34:42 +0700

    small change

--- src/states/PlayState.cpp
+++ src/states/PlayState.cpp
@@ -88,7 +88,7 @@ void PlayState::Enter()
     // auto& testHoming2 = testObj2.AddComponent<HomingComponent>(m_pPlayerObject, 1.0f);
     
     // this->CreateMinitaurEnemy();
-    // this->CreateHarpyEnemy();
+    this->CreateHarpyEnemy();
 }
 
 void PlayState::Exit()
