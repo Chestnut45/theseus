737f4d63c187eada5e386a4cd0e129ffa2854b4c
tripzcodes <151476551+tripzcodes@users.noreply.github.com>
2024-11-19 11:27:29 -0400

    attackdamage component reset

--- src/components/AttackDamageComponent.h
+++ src/components/AttackDamageComponent.h
@@ -3,32 +3,21 @@
 // Original Author: Nguyễn Minh Nhật
 // Checks for collision with HURTBOXDRs if owner object has HURTBOXDD.
 //-----------------------------------------------------------------------------
+
 #pragma once
 
 #include <wolf.h>
 #include "../ColliderManager.h"
-#include "../inventory/WeaponItem.h"
-#include "AttackSourceType.h"
 
-class AttackDamageComponent : public wolf::BaseComponent {
+class AttackDamageComponent : public wolf::BaseComponent
+{
 public:
-    // Constructor for weapon-based attack
-    AttackDamageComponent(float p_damage, ColliderManager* p_collider_manager, WeaponType p_weaponType);
-
-    // Constructor for non-weapon-based attack (e.g., Harpy, ThrowableObject)
-    AttackDamageComponent(float p_damage, ColliderManager* p_collider_manager, AttackSourceType p_sourceType);
-
+    AttackDamageComponent(float p_damage, ColliderManager* p_collider_manager);
     virtual ~AttackDamageComponent();
 
     void Update(float p_dt);
 
 private:
     float m_fDamage = 0.0f;
-    ColliderManager* m_pColliderManager = nullptr;
-    WeaponType m_weaponType;
-    AttackSourceType m_sourceType;
-    bool m_isWeaponAttack = false;  // Flag to indicate if the attack is weapon-based
-
-    // Helper method to apply knockback based on weapon or source type
-    void ApplyKnockback(wolf::GameObject* target, const glm::vec2& direction);
+    ColliderManager * m_pColliderManager = nullptr;
 };
