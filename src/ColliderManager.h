903d97311cb333f3fbb49cf9893b64386fc9fb12
tripzcodes <151476551+tripzcodes@users.noreply.github.com>
2024-12-01 18:00:30 -0400

    fixe

--- src/ColliderManager.h
+++ src/ColliderManager.h
@@ -47,6 +47,7 @@ private:
    // InternalUse methods for hitbox sliding response
     bool IsCollidingInternalUse(ColliderComponent& p_colliderComponent1, ColliderComponent& p_colliderComponent2, float p_delta);
     bool CustomAABBInternalUse(const glm::vec2& p_translation_1, const glm::vec2& p_translation_2, const glm::vec2& p_dimensions_1, const glm::vec2& p_dimensions_2, VelocityComponent* p_velocity_1, VelocityComponent* p_velocity_2, float p_delta);
-
+    void CheckCornerCollision(float p_delta);
+    bool HandleCornerCollision(ColliderComponent* collider1, glm::vec2& velocity1, const std::array<glm::vec2, 4>& corners, ColliderComponent* collider2, const glm::vec2& scale1,const glm::vec2& translation1);
     std::vector<wolf::GameObjectID> m_vToBeDestroyed;
 };
