c8651c922fbc50d6b95b1e23ee218fab3c5ee462
Chestnut45 <cpt20xx@gmail.com>
2025-01-27 14:04:41 -0400

    fix player hitbox offset

--- src/states/PlayState.cpp
+++ src/states/PlayState.cpp
@@ -626,7 +626,7 @@ void PlayState::CreatePlayer()
     auto& inventory = m_pPlayerObject->AddComponent<PlayerInventoryComponent>(16, 4, ImVec2(50, 50));
 
     auto& collider = m_pPlayerObject->AddComponent<ColliderComponent>(ColliderComponent::ColliderType::HITHURTBOXDR, 0, 1);
-    collider.AddColliderBox(glm::vec2(7.0f, 10.0f), glm::vec2(-4.0f, -2.0f));
+    collider.AddColliderBox(glm::vec2(7.0f, 10.0f), glm::vec2(-4.0f, -3.0f));
 
     // Add health
     auto& health = m_pPlayerObject->AddComponent<HealthComponent>(1000);
