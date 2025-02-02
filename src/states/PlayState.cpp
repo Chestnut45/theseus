98dac92ec6b5fadbc990f3e33374410e8402a4f8
Chestnut45 <cpt20xx@gmail.com>
2025-02-01 22:07:59 -0400

    adjust volume of spike to reasonable level

--- src/states/PlayState.cpp
+++ src/states/PlayState.cpp
@@ -843,7 +843,7 @@ void PlayState::OnTriggerEvent(const TriggerEvent& event) {
         case TriggerPurpose::SPIKE_TRAP: {
 
             // Play sound effect
-            wolf::Audio::Play("data/sounds/sfx_spike_trap.wav", 0.75f);
+            wolf::Audio::Play("data/sounds/sfx_spike_trap.wav", 0.4f);
 
             auto& trapObj = m_pGameInstance->GetScene().CreateObject2D();
             auto& trapSprite = trapObj.AddComponent<wolf::Sprite2D>("data/textures/SpikesExtended.png");
