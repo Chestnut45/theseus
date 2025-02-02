2190335d73aad2e39768b78bbe8485db443619f9
Chestnut45 <cpt20xx@gmail.com>
2025-02-02 00:05:22 -0400

    remix spike trap sfx (low pass)

--- src/states/PlayState.cpp
+++ src/states/PlayState.cpp
@@ -843,7 +843,7 @@ void PlayState::OnTriggerEvent(const TriggerEvent& event) {
         case TriggerPurpose::SPIKE_TRAP: {
 
             // Play sound effect
-            wolf::Audio::Play("data/sounds/sfx_spike_trap.wav", 0.4f);
+            wolf::Audio::Play("data/sounds/sfx_spike_trap.wav", 0.65f);
 
             auto& trapObj = m_pGameInstance->GetScene().CreateObject2D();
             auto& trapSprite = trapObj.AddComponent<wolf::Sprite2D>("data/textures/SpikesExtended.png");
