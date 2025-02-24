caa8759eff92330f5787da2070e6e229ed643989
Chestnut45 <cpt20xx@gmail.com>
2025-02-24 17:09:07 -0400

    final

--- src/components/BossController.cpp
+++ src/components/BossController.cpp
@@ -691,7 +691,7 @@ void BossController::SpawnWave(int waveIndex)
 
     // Load enemy data
     EnemyDataLoader loader;
-    loader.LoadAllEnemyData("data/enemies.yaml");
+    loader.LoadAllEnemyData("data/enemies_bossfight.yaml");
 
     EnemyData minitaurData = loader.LoadEnemyData("minitaur");
     EnemyData harpyData = loader.LoadEnemyData("harpy");
