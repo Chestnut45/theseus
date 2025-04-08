661361497aa275e32d3be654ce56e483db08533e
Chestnut45 <cpt20xx@gmail.com>
2025-04-08 15:45:24 -0300

    fix fluid room sizes and implement fireball animations

--- src/components/HarpyController.cpp
+++ src/components/HarpyController.cpp
@@ -380,8 +380,8 @@ void HarpyController::HandleAttackingState(float delta)
 
             auto& attackDamageComponent = projectile.AddComponent<AttackDamageComponent>(m_baseDamage, m_pColliderManager,0.0f,statusEffects, GetGameObject());
 
-            auto& projectileSprite = projectile.AddComponent<wolf::Sprite2D>("data/textures/Fireball.png");
-            projectileSprite.SetOriginToCenterOfTexture();
+            auto& projectileSprite = projectile.AddComponent<AnimatedSprite2D>("data/fireball_anim_init.yaml");
+            projectileSprite.SetLayer(20);
             
             auto& projectileCollider = projectile.AddComponent<ColliderComponent>(ColliderComponent::ColliderType::HURTBOXDD, 1, 1);
             projectileCollider.AddColliderBox(projectileDimensions, hurtboxOffset);
