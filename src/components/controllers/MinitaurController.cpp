b769048a42004f6c0ada6cb139474060a45b7c9c
Chestnut45 <cpt20xx@gmail.com>
2025-04-13 01:12:47 -0300

    IT'S FINALLY OVER (emotes are fixed)

--- src/components/controllers/MinitaurController.cpp
+++ src/components/controllers/MinitaurController.cpp
@@ -79,17 +79,13 @@ void MinitaurController::Init(const EnemyData& data)
     m_pEmoteObj = &pGameObject->GetScene().CreateObject2D();
     pGameObject->AddChild(*m_pEmoteObj);
     wolf::Transform2D* transform = m_pEmoteObj->GetComponent<wolf::Transform2D>();
-    transform->SetPosition(glm::vec2(-8.0f, 8.0f));
+    transform->SetPosition(glm::vec2(0.0f, 16.0f));
 
     // Add emotes spritesheet
     AnimatedSprite2D* emotesSpritesheet = &m_pEmoteObj->AddComponent<AnimatedSprite2D>("data/animations/emotes_anim_init.yaml");
     emotesSpritesheet->SetAnimPaused(true);
-    emotesSpritesheet->SetOriginToCenterOfFrame();
     emotesSpritesheet->SetLightingEnabled(false);
     emotesSpritesheet->SetLayer(100);
-
-    // Initialise emotes-related variables
-    m_fEmoteTimer = EMOTE_TIME;
 
     // Find the PathfindingManager in the scene
     bool pathfindingManagerFound = false;
